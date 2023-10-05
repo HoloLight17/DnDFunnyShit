611827b7ab82d4c94f42d1fddccb9e7691315556
HoloLight17 <uselesszamir@gmail.com>
2023-10-05 17:31:11 +0300

    small improvements

--- src/main/java/dnd/Main.java
+++ src/main/java/dnd/Main.java
@@ -1,31 +1,7 @@
 package dnd;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        writeToFile(characteristicsRoller());
-    }
-
-    public static List<Integer> characteristicsRoller() {
-        List<Integer> finalResult = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            List<Integer> dicesList = new ArrayList<>(Arrays.stream(new Random().ints(4, 1, 7).toArray()).boxed().toList());
-            dicesList.remove(Collections.min(dicesList));
-            finalResult.add(dicesList.stream().reduce(0, Integer::sum));
-        }
-        return finalResult;
-    }
-
-    public static void writeToFile(Object... ints) {
-        Path path = Paths.get("chars.txt");
-        try {
-            Files.write(path, Arrays.toString(ints).getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Characteristics.generate();
     }
 }
