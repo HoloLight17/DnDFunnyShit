c041e841ad2823b5ace0abfcfbf47633137bff28
HoloLight17 <uselesszamir@gmail.com>
2023-10-24 15:20:09 +0300

    rumors

---
+++ src/main/java/dnd/Main.java
@@ -0,0 +1,17 @@
+package dnd;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/text.txt"));
+        List<String> list = reader.lines().toList();
+        reader.close();
+        System.out.println(list.get(new Random().nextInt(list.size())));
+    }
+}
