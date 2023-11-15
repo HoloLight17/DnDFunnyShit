e1aea0c1ac2619de8e66e7f16a7e86364e928df9
spechurkin <uselesszamir@gmail.com>
2023-11-15 18:30:02 +0300

    sort markdown table

--- src/main/java/dnd/Main.java
+++ src/main/java/dnd/Main.java
@@ -1,8 +1,42 @@
 package dnd;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        Characteristics.generate();
-        System.out.println(HitPoints.generate(17, 6));
+//        System.out.println(Characteristics.generate());
+//        System.out.println(HitPoints.generate(7, 8));
+//        System.out.println(HitPoints.randomizeHPOnce(8, 2));
+        File sortedFile = new File("sorted_table.txt");
+        if (sortedFile.exists()) {
+            sortedFile.delete();
+        }
+        List<Integer> indexes = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+        Map<Integer, String> sortedTable = new TreeMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("table.txt"));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(sortedFile, true))) {
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                String[] splitLine = line.split("\\|");
+                indexes.add(Integer.valueOf(splitLine[1]));
+                values.add(splitLine[2]);
+            }
+            values = values.stream().sorted().toList();
+            for (int i = 0; i < indexes.size(); ) {
+                sortedTable.put(indexes.get(i), values.get(i));
+                i++;
+            }
+            for (Integer index : sortedTable.keySet()) {
+                bw.append("|").append(String.valueOf(index)).append("|").append(sortedTable.get(index)).append("|");
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
