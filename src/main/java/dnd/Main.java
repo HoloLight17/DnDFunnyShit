ceebc921db0b8d8e44e09afb8ad5344e1dac5a62
spechurkin <uselesszamir@gmail.com>
2023-11-28 19:06:24 +0300

    refactor

--- src/main/java/dnd/Main.java
+++ src/main/java/dnd/Main.java
@@ -1,6 +1,8 @@
 package dnd;
 
+import dnd.properties.Properties;
 import dnd.properties.benefits.MajorBenefits;
+import dnd.properties.benefits.MinorBenefits;
 
 public class Main {
     public static void main(String[] args) {
